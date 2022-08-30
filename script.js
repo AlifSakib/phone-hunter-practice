002c27c44af5d556130adb312c89320d8e6db756
AlifSakib <alifsakib@gmail.com>
2022-08-30 14:14:05 +0600

    added footer and navbar

--- script.js
+++ script.js
@@ -57,7 +57,8 @@ const displayData = (phones, limit) => {
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
         <button
       onclick="modalShow() ; playerDetails('${slug}')"
-      class="block w-full md:w-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      class="block w-full md:w-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium
+      rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       type="button"
       data-modal-toggle="small-modal"
     >
@@ -82,6 +83,7 @@ const processSearch = (limit) => {
 
 // Search By Mouse Click
 document.getElementById("search-btn").addEventListener("click", function (e) {
+  hideBlogs();
   e.preventDefault();
   processSearch(10);
   const showAll = document.getElementById("show-all");
@@ -136,19 +138,21 @@ const diplayDetails = (phone) => {
             class="flex justify-between items-center p-5 rounded-t border-b dark:border-gray-600">
             <h3 class="text-xl font-medium text-gray-900 dark:text-white">${name}</h3>
             <button
-              type="button"
-              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-              data-modal-toggle="small-modal">
-              <svg aria-hidden="true"
+                type="button"
+                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center
+                 dark:hover:bg-gray-600  dark:hover:text-white"
+                data-modal-toggle="small-modal">
+            <svg aria-hidden="true"
                 class="w-5 h-5"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fill-rule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"></path>
-              </svg>
+            <path
+                fill-rule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 
+                0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clip-rule="evenodd"></path>
+            </svg>
               <span class="sr-only">Close modal</span>
             </button>
           </div>
@@ -179,9 +183,12 @@ const diplayDetails = (phone) => {
               onclick="modalHide()"
               data-modal-toggle="small-modal"
               type="button"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cancle</button>
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5
+               text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cancle</button>
             <button data-modal-toggle="small-modal" type="button"
-              class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+              class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm 
+              font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white
+               dark:hover:bg-gray-600 dark:focus:ring-gray-600">
               Order Now
             </button>
           </div>
@@ -199,3 +206,8 @@ const modalHide = () => {
   const modal = document.getElementById("small-modal");
   modal.classList.add("hidden");
 };
+
+function hideBlogs() {
+  const blogs = document.getElementById("blogs");
+  blogs.classList.add("hidden");
+}
