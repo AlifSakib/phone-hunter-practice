9d5c6c3b09fa197cb5e35a13f6571c2708669dba
AlifSakib <alifsakib@gmail.com>
2022-08-31 00:41:25 +0600

    done

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ const displayData = (phones, limit) => {
         </a>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
         <button
-      onclick="modalShow() ; playerDetails('${slug}')"
+      onclick="playerDetails('${slug}')"
       class="block w-full md:w-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium
       rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       type="button"
@@ -69,7 +69,6 @@ const displayData = (phones, limit) => {
       </div>   
     `;
     phoneContainer.appendChild(phoneDiv);
-    hideBlogs();
   });
 
   toggleSpinner();
@@ -194,10 +193,12 @@ const diplayDetails = (phone) => {
           </div>
         </div>
   `;
+  modalShow();
 };
 
 //Modal Show n Hide
 const modalShow = () => {
+  console.log("click");
   const modal = document.getElementById("small-modal");
   modal.classList.remove("hidden");
 };
@@ -206,8 +207,3 @@ const modalHide = () => {
   const modal = document.getElementById("small-modal");
   modal.classList.add("hidden");
 };
-
-function hideBlogs() {
-  const blogs = document.getElementById("blogs");
-  blogs.classList.add("hidden");
-}
